1838c9b182dd2a0e47dd052075c95d202d492831
EmreTekinalp <etekinalp@outlook.com>
2015-12-27 11:52:05 -0800

    Base implementation of GameEngine Project

--- projects/Engine/Engine/Engine.cpp
+++ projects/Engine/Engine/Engine.cpp
@@ -1,14 +1,7 @@
 // Engine.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#include <iostream>
-using std::cout;
-using std::endl;
-
-int main()
+void main()
 {
-	cout << "Hello World!" << endl;
-    return 0;
-}
 
+}
