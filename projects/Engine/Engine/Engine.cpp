a43ddfef599d23279919a9291a8fc70eedd75443
EmreTekinalp <etekinalp@outlook.com>
2015-12-27 11:36:42 -0800

    First setup of GameEngine

---
+++ projects/Engine/Engine/Engine.cpp
@@ -0,0 +1,14 @@
+// Engine.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+    return 0;
+}
+
